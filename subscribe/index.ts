5f004e390fa4d2f19abd24fa44e0446ba2728f83
Dylan On <dylanon@users.noreply.github.com>
2019-07-13 20:35:08 -0400

    refactor: 💡 remove commented code

--- subscribe/index.ts
+++ subscribe/index.ts
@@ -45,26 +45,4 @@ export default async function(req: NowRequest, res: NowResponse) {
       return res.json({ error: e });
     });
   return res.json({ message: "List member updated to 'subscribed'." });
-
-  // try {
-  //   const member = await listMember.get();
-  //   // Already in system
-  //   const { status } = member;
-  //   // If subscribed, bail and send success
-  //   if (status === MemberStatus.Subscribed) {
-  //     return res.json({ message: 'Already subscribed.' });
-  //   } else {
-  //     // If unsubscribed/pending/cleaned, update status to 'subscribed'
-  //     // TODO: PATCH member status
-  //     await listMember.update({
-  //       status: MemberStatus.Subscribed,
-  //     });
-  //     return res.json({ message: "List member updated to 'subscribed'." });
-  //   }
-  // } catch (error) {
-  //   // Not found in system
-  //   // If not found, add contact to audience
-  //   listMember.create(MemberStatus.Subscribed);
-  //   return res.json({ error: error });
-  // }
 }
